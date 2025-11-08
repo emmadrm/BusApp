bf1a8ac30183aa52c483bb01feaf33d57d1ace47
emma <emmaderm10@gmail.com>
2025-11-08 19:39:20 +0200

    /delivery front-end

---
+++ src/components/Menu.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Menu(props) {
+    return (
+        <div> 
+        </div>
+    );
+}
+
+export default Menu;
---
+++ src/components/Store.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Store(props) {
+    return (
+        <div>
+            <div className="store_list">
+                <div className="store-card" >  
+                    <a href="/" className="store-item"> /* Link gia magazi */
+                        <img className="logo" src={props.logo} alt={`${props.name} logo`} loading="lazy" />
+                        <h4>{props.name}</h4>
+                        <p>{props.category}</p>
+                        <p>{props.offer}</p>
+                        <small>{props.time}</small>
+                    </a>  
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+export default Store;
